af34f2450eb9acd9f3a76300aa819ce4dbc3a3d0
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-12-24 19:28:56 +0530

    solution module update

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -37,11 +37,17 @@ impl SolutionFactory {
         let mut solutions = HashMap::new();
         register_solution!(solutions, p007::Problem007);
         register_solution!(solutions, p009::Problem009);
+        register_solution!(solutions, p016::Problem016);
+        register_solution!(solutions, p011::Problem011);
         register_solution!(solutions, p004::Problem004);
+        register_solution!(solutions, p012::Problem012);
+        register_solution!(solutions, p013::Problem013);
         register_solution!(solutions, p005::Problem005);
         register_solution!(solutions, p006::Problem006);
+        register_solution!(solutions, p014::Problem014);
         register_solution!(solutions, p002::Problem002);
         register_solution!(solutions, p010::Problem010);
+        register_solution!(solutions, p015::Problem015);
         register_solution!(solutions, p003::Problem003);
         register_solution!(solutions, p008::Problem008);
         register_solution!(solutions, p001::Problem001);
@@ -73,11 +79,17 @@ impl SolutionFactory {
 
 pub mod p007;
 pub mod p009;
+pub mod p016;
+pub mod p011;
 pub mod p004;
+pub mod p012;
+pub mod p013;
 pub mod p005;
 pub mod p006;
+pub mod p014;
 pub mod p002;
 pub mod p010;
+pub mod p015;
 pub mod p003;
 pub mod p008;
 pub mod p001;
